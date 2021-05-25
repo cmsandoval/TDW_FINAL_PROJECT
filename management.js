2e5eaa9b3b6fcbdf4902e902eacad58607005b08
christian <cm.sandoval@alumnos.upm.es>
2021-05-26 00:57:54 +0200

    put method to each user fixed

--- management.js
+++ management.js
@@ -1,56 +1,129 @@
 let access_url = "http://127.0.0.1:8000/";
 let api_url = "http://127.0.0.1:8000/api/v1/";
 let httpRequest =
-	new XMLHttpRequest();
-    let users_endpoint = "users";
+    new XMLHttpRequest();
+let users_endpoint = "users";
+let etag;
+let userId;
 
-function onLoad(){
-    request(api_url, users_endpoint, getUsersResponse, 'GET',undefined);
+function onLoad() {
+    request(api_url, users_endpoint, getUsersResponse, 'GET', undefined);
 }
 
 function request(url, endpoint, response, method, params) {
-	httpRequest.open(method, encodeURI(url + endpoint), true);
-	httpRequest.responseType = "json";
-	httpRequest.onload = response;
-	let jwt = window.localStorage.getItem("jwt");
-	if (jwt != null) {
-		httpRequest.setRequestHeader('Authorization', "Bearer " + jwt);
-	}
-	if (params === undefined) {
-		httpRequest.send();
-	} else {
-		httpRequest.send(params);
-	}
+    httpRequest.open(method, encodeURI(url + endpoint), true);
+    httpRequest.responseType = "json";
+    httpRequest.onload = response;
+    let jwt = window.localStorage.getItem("jwt");
+    if (jwt != null) {
+        httpRequest.setRequestHeader('Authorization', "Bearer " + jwt);
+    }
+    if (params === undefined) {
+        httpRequest.send();
+    } else {
+        httpRequest.send(params);
+    }
 }
 
-function getUsersResponse(){
-    switch(this.status){
+function getUsersResponse() {
+    switch (this.status) {
         case 200: successGetUsers(this.response.users);
-        break;
+            break;
         default: alert(this.response.message);
     }
 }
 
-function successGetUsers(usersList){
+function updateUser(role, etag) {
+    let jwt = window.localStorage.getItem("jwt");
+    
+    if (httpRequest.status == 200) {
+        if (etag != undefined) {
+            fetch(api_url + users_endpoint + "/" + userId, {
+                method: 'PUT',
+                body: JSON.stringify(role),
+                headers: {
+                    'accept': 'application/json',
+                    'Content-Type': 'application/json',
+                    'Accept-Encoding': 'gzip, deflate, br',
+                    'Authorization': "Bearer " + jwt,
+                    'If-Match': etag
+                }
+            }).then(res => successRequest())
+                .catch(error => console.error('Error:', error))
+                .then(response => console.log('Success:', response));
+        }
+    }
+}
+
+function successRequest() {
+    alert("user updated");
+    window.location.reload();
+}
+
+function getRole(userId) {
+    var writer = document.getElementById('writer_' + userId);
+    var reader = document.getElementById('reader_' + userId);
+    let scope = "";
+    var resul;
+    if (writer.checked) {
+        scope = "writer";
+        resul = { role: scope };
+    } else if (reader.checked) {
+        scope = "reader";
+        resul = { role: scope };
+    } else {
+        alert("Needs to choose one option");
+    }
+    return resul;
+}
+
+function getUserId() {
+    userId = this.value;
+    let role = getRole(userId);
+    if (role !== undefined) {
+        getUserIdService(userId, role);
+    }
+}
+
+function getUserIdService(userId, role){
+    let jwt = window.localStorage.getItem("jwt");
+    fetch(api_url + users_endpoint + "/" + userId, {
+        method: 'GET',
+        headers: {
+            'accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Accept-Encoding': 'gzip, deflate, br',
+            'Authorization': "Bearer " + jwt
+        }
+    }).then(res =>  updateUser(role, res.headers.get("etag")))
+       // updateUser(role, res.response.getResponseHeader('ETag'))
+       .then(response => console.log('Success:', response))
+        .catch(error => console.error('Error:', error));
+}
+
+function successGetUsers(usersList) {
     usersList.forEach((item) => {
         showData(item.user);
+        etag = httpRequest.getResponseHeader('ETag');
     });
 }
 
-function showData(user){
+function showData(user) {
     let body = document.getElementById('table_body');
 
     var tr = document.createElement('tr');
-    var th =  document.createElement('th');
+    var th = document.createElement('th');
     th.setAttribute('scope', 'row');
     th.innerHTML = user.id;
+
     var tdUserName = document.createElement('td');
     tdUserName.innerHTML = user.username;
+
     var tdEmail = document.createElement('td');
     tdEmail.innerHTML = user.email;
-
+    //------------
     var tdOptions = document.createElement('td');
-    tdOptions.setAttribute('colspan', '2');
+
     var divMain = document.createElement('div');
     divMain.classList.toggle('form-outline');
     divMain.classList.toggle('mb-4');
@@ -61,12 +134,12 @@ function showData(user){
     inputReader.classList.toggle('form-check-input');
     inputReader.setAttribute('type', 'checkbox');
     inputReader.setAttribute('name', 'reader');
-    inputReader.setAttribute('value', user.id);
+    inputReader.setAttribute('id', 'reader_' + user.id);
     var labelReader = document.createElement('label');
     labelReader.classList.toggle('form-check-label');
     labelReader.setAttribute('for', 'flexCheckDefault');
     labelReader.innerHTML = 'Reader';
-    if(user.role.includes('reader')){
+    if (user.role.includes('reader')) {
         inputReader.setAttribute('checked', true);
     }
 
@@ -75,16 +148,28 @@ function showData(user){
     var inputWriter = document.createElement('input');
     inputWriter.classList.toggle('form-check-input');
     inputWriter.setAttribute('type', 'checkbox');
-    inputWriter.setAttribute('name', 'reader');
-    inputWriter.setAttribute('value', user.id);
-    if(user.role.includes('writer')){
+    inputWriter.setAttribute('name', 'writer');
+    inputWriter.setAttribute('id', 'writer_' + user.id);
+    if (user.role.includes('writer')) {
         inputWriter.setAttribute('checked', true);
     }
+
     var labelWriter = document.createElement('label');
     labelWriter.classList.toggle('form-check-label');
     labelWriter.setAttribute('for', 'flexCheckDefault');
-    labelWriter.innerHTML = 'Reader';
-    
+    labelWriter.innerHTML = 'Writer';
+    //--------------
+
+    var tdEditButton = document.createElement('td');
+    var buttonEditar = document.createElement("button");
+    buttonEditar.setAttribute("type", "button");
+    buttonEditar.setAttribute('value', user.id);
+    buttonEditar.classList.toggle("btn");
+    buttonEditar.classList.toggle("btn-warning");
+    buttonEditar.innerHTML = "Update";
+    buttonEditar.style.display = 'block';
+    buttonEditar.addEventListener('click', getUserId);
+
 
     divWriter.appendChild(labelWriter);
     divWriter.appendChild(inputWriter);
@@ -93,9 +178,11 @@ function showData(user){
     divMain.appendChild(divWriter);
     divMain.appendChild(divReader);
     tdOptions.appendChild(divMain);
+    tdEditButton.appendChild(buttonEditar);
     tr.appendChild(th);
     tr.appendChild(tdUserName);
     tr.appendChild(tdEmail);
     tr.appendChild(tdOptions);
+    tr.appendChild(tdEditButton);
     body.appendChild(tr);
 }
